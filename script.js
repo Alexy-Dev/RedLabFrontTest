1f16e4cf18c28d939552f93f388c6c946f0cacaf
Oleksii Polonchuk <63574876+Alexy-Dev@users.noreply.github.com>
2021-07-09 23:16:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const round = document.querySelector('.btn-prim-desc');
+    const nav = document.querySelector('.nav'),
+          navback = document.querySelector('.nav-back');
 
     function openAnimation() {                  //засовываем алгоритм открывания в функцию
     round.classList.add('ZoomOut');
-    round.classList.add('rotate-ell');  
+    round.classList.add('rotate-ell');
+    nav.classList.add('pose');
+    navback.classList.add('hide'); 
 }
     const move = document.querySelector('.plaseholder');
     function openAnimationFooter() {                  
-    move.classList.add('MooveFooter');      
+    move.classList.add('MooveFooter');
+    round.classList.add('ZoomOutFooter');      
 }
     const moveRest = document.querySelector('.rectangle_130');
     function openAnimationRest() {                  
     moveRest.classList.add('MooveFooterRest');
-    moveRest.classList.remove('hide');
-    moveRest.classList.add('show');      
+    // setTimeout(() => {
+    //     moveRest.classList.remove('hide');
+    //     moveRest.classList.add('show');
+    // }, 7000);      
 } 
+    const tagsShow = document.querySelector('.footer-scroll');
+    function openAnimationTags() {
+        tagsShow.classList.add('ZoomInTags');
+          setTimeout(() => {      
+        // tagsShow.classList.remove('hide');
+        tagsShow.classList.add('show');
+        }, 7000);        
+    }
     function showAnimationByScroll() {
                 if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight/2) { //для события по прокрутке экрана до середины
                     openAnimation();
@@ -23,11 +38,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
                 if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { //для события по прокрутке экрана до конца
                     openAnimationFooter();
-                    // window.removeEventListener('scroll', showAnimationByScroll);
-                }
-                if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { //для события по прокрутке экрана до конца
                     openAnimationRest();
-                    // window.removeEventListener('scroll', showAnimationByScroll);
+                    openAnimationTags();                
                 }
             }
             window.addEventListener('scroll', showAnimationByScroll);
