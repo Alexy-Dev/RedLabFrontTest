a6ddca104dd53edd01c5ccc61f4a465f914ef83e
Oleksii Polonchuk <63574876+Alexy-Dev@users.noreply.github.com>
2021-07-16 13:34:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const nav = document.querySelector('.nav'),
           navback = document.querySelector('.nav-back');
 
-    function openAnimation() {                  //засовываем алгоритм открывания в функцию
+    function openAnimation() {                  
     round.classList.add('ZoomOut');
     round.classList.add('rotate-ell');
     nav.classList.add('pose');
@@ -18,9 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const moveRest = document.querySelector('.rectangle_130');
     function openAnimationRest() {                  
     moveRest.classList.add('MooveFooterRest');
-    // moveRest.classList.remove('pose');
+    
     setTimeout(() => {      
-        // tagsShow.classList.remove('hide');
+        
         moveRest.classList.add('show');
         }, 2000);       
     } 
@@ -28,9 +28,17 @@ window.addEventListener('DOMContentLoaded', () => {
     function openAnimationTags() {
         tagsShow.classList.add('ZoomInTags');
           setTimeout(() => {      
-        // tagsShow.classList.remove('hide');
+        
         tagsShow.classList.add('show');
-        }, 6000);        
+        }, 5000);        
+    }
+    const tagsShow2 = document.querySelector('.footer-scroll-2');
+    function openAnimationTags2() {
+        tagsShow2.classList.add('ZoomInTags');
+          setTimeout(() => {      
+        // tagsShow.classList.remove('hide');
+        tagsShow2.classList.add('show');
+        }, 3000);        
     }
     const feature02 = document.querySelector('.feature02'),
           feature03 = document.querySelector('.feature03'),
@@ -45,13 +53,10 @@ window.addEventListener('DOMContentLoaded', () => {
           tabs03 = document.querySelector('.tabs-bath'),
           tabs04 = document.querySelector('.tabs-hall');
     function openTabs() {
-        // tabs01.classList.add('MooveTabs01');
-        // tabs01.classList.add('show');
         tabs02.classList.add('MooveTabs02');
         setTimeout(() => {
             tabs02.classList.add('show');
-            }, 1000);  
-        
+            }, 1000);        
         tabs03.classList.add('MooveTabs03');
         setTimeout(() => {
             tabs03.classList.add('show');
@@ -59,7 +64,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tabs04.classList.add('MooveTabs04');
         setTimeout(() => {
             tabs04.classList.add('show');
-            }, 3000);  
+            }, 2200);  
     }
     function showAnimationByScroll() {
                 if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight/2) { //для события по прокрутке экрана до середины
@@ -74,4 +79,56 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             }
             window.addEventListener('scroll', showAnimationByScroll);
+    //Tabs
+
+    const  tabsItem = document.querySelectorAll('.tab'),
+          tabsParent = document.querySelector('.tabs');
+    
+    const slider = document.querySelector('.container-img'),
+          slidesParent = document.querySelector('.rectangle-img'),
+          img = document.querySelectorAll('.img');
+    
+    function hideTab() {              
+        tabsItem.forEach(item => {            
+            item.classList.remove('tabs_active');
+            item.classList.remove('hide');          
+        });
+        slidesParent.classList.remove('MooveImg01');
+    }
+
+    function showTab(i = 0) {
+        tabsItem[i].classList.add('tabs_active');
+        console.log(tabsItem[i]);
+    }
+
+    hideTab();
+    showTab();      
+ 
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        
+        img.forEach((item, i) => {
+            if (i = 0, i <10, i++) {
+                i++;
+                item[i];                
+            }
+        });
+        
+
+        if (target && target.classList.contains('tab')) {
+            tabsItem.forEach((item, i) => {
+                if (target == item) {
+                    hideTab();
+                    showTab(i);
+                    slider.classList.add('show');
+                    console.log(img[i]);
+                    img[i].classList.add('MooveImg03');
+                }
+                if (target && target.classList.contains('tabs-bath')) {
+                    img[2].classList.add('MooveImg01');
+                    openAnimationTags2();
+                }              
+            });
+        }
+    });
 });
